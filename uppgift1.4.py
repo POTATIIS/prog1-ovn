5d8852cbf33744091a063b354dd643b051352226
POTATIIS <alexander.martinsson@elev.ga.ntig.se>
2020-08-31 14:42:38 +0200

    Variable övning

--- uppgift1.4.py
+++ uppgift1.4.py
@@ -1,4 +1,4 @@
 Alexander = input('Vad heter du? ')
 print('hej', Alexander)
-efternamn('martinsson') = input('vad heter du i efternamn?')
-print('mitt efternamn är', martinsson )
+Martinsson = input('vad heter du i efternamn?')
+print('mitt efternamn är', Martinsson)
