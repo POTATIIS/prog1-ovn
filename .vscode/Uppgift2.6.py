933617a6374a136ecb9ee3093a385b9b6d17c745
POTATIIS <alexander.martinsson@elev.ga.ntig.se>
2020-09-21 14:24:40 +0200

    TID

---
+++ .vscode/Uppgift2.6.py
@@ -0,0 +1,7 @@
+svar = input('skriv antalet sekunder: ')
+sek = int(svar)
+tim = sek // 3600
+sek = sek % 3600
+min = sek // 60
+sek = sek % 60
+print(f' \nTimmar: {tim} \nMinuter {min} \nSekunder {sek}')
