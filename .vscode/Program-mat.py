c8960682fc6224010dbbdc859f093e99f359c8b7
POTATIIS <alexander.martinsson@elev.ga.ntig.se>
2020-10-12 13:35:59 +0200

    wake up routine
    OOF

---
+++ .vscode/Program-mat.py
@@ -0,0 +1,10 @@
+eat = input('Var vill du äta?')
+
+if eat == 'Olles':
+    print('Du äter, Gratis')
+elif eat == 'max':
+    print('Du sparar inte på studiebiebidraget')
+if eat == 'Coop':
+    print('Dagens motion!')
+else:
+    print('KEK 😂')    
